61eefe56bd8c38082bb49c4910ec96593646c9c1
AnnickS <32581380+AnnickS@users.noreply.github.com>
2020-07-24 20:01:41 -0700

    Edited About Section

--- src/mainPage.js
+++ src/mainPage.js
@@ -82,28 +82,35 @@ class mainPage extends React.Component {
               </div>
               <div class="row">
                 <div class="col-lg-4 col-lg-offset-2">
-                  <p>
-                    I am a software developer interested in video game
-                    development. I have over a year's experience developing in the
-                    Unity Game Engine, and have worked with C#, React, and Java.
-                    Go ahead and check out some of my examples above!
-                  </p>
+                  <p>I am a software developer with a Bachelor of Science 
+                    in Computer Science interested in video game and VR/AR 
+                    development. I have over a year experience developing 
+                    in the Unity Game Engine, and have worked with C#, 
+                    React, AWS, and Java.</p>
+                  <p>This portfolio page is my own personal project developed 
+                    with React and hosted on AWS using S3 and CloudFront. Go 
+                    ahead and check out some of my other projects above!</p>
                 </div>
                 <div class="col-lg-4">
-                  <p>
-                    As an assistant working at the Bellevue College XR Lab I
-                    construct workshops that are presented to classes across a
-                    variety of vocations to help facilitate student learning,
-                    while maintaining a focus on Virtual Reality.
-                  </p>
+                  <p>At my last job as an assistant working at the Bellevue 
+                    College XR Lab, I was able to work alongside the most 
+                    passionate individuals with the goal of spreading the use 
+                    of VR in education. I was responsible for constructing 
+                    workshops that were presented to classes from across a 
+                    variety of vocations to help facilitate student learning 
+                    while maintaining a focus on virtual reality.</p>
+                  <p>In my time there I developed a “Video Game Design in VR” 
+                    workshop, focusing on the differences of VR game development 
+                    in comparison with regular game design.</p>
                 </div>
                 <div class="col-lg-4">
-                  <p>
-                    In my spare time I play Dungeons and Dragons with friends,
-                    game socially, and write short vignettes. My friends and
-                    family are of the utmost importance to me, so when I can I
-                    spend my time visiting and connecting with others.
-                  </p>
+                  <p>In my spare time I play Dungeons and Dragons with friends, 
+                    practice ukulele, and create stories. I enjoy drawing, writing, 
+                    and worldbuilding to further embellish the stories I design.</p>
+                  <p>Friends and family are of utmost important to me, so I enjoy 
+                    cooking and creating memorable experiences with them. My 
+                    goal is to fill my life with amazing memories, and to 
+                    create worthwhile experiences for others to enjoy.</p>
                 </div>
               </div>
             </div>
